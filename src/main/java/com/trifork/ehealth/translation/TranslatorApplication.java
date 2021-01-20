216ff47500d8a991e631faf19506af8aa56acbe6
ttn <ttn@trifork.com>
2021-01-20 09:39:35 +0100

    Renamed Application class to align with project name

--- src/main/java/com/trifork/ehealth/translation/TranslatorApplication.java
+++ src/main/java/com/trifork/ehealth/translation/TranslatorApplication.java
@@ -1,32 +1,27 @@
 package com.trifork.ehealth.translation;
 
-import com.trifork.ehealth.translation.fhir.annotations.OnDSTU3Condition;
-import com.trifork.ehealth.translation.fhir.dstu3.TranslationRunnerDstu3;
-import com.trifork.ehealth.translation.google.GoogleTranslator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 
 import java.io.IOException;
 
 @SpringBootApplication
-public class CodesystemTranslatorApplication implements CommandLineRunner {
-    private static final Logger logger = LoggerFactory.getLogger(CodesystemTranslatorApplication.class);
+public class TranslatorApplication implements CommandLineRunner {
+    private static final Logger logger = LoggerFactory.getLogger(TranslatorApplication.class);
     private final ApplicationContext applicationContext;
     private final TranslationRunner translationRunner;
 
-    public CodesystemTranslatorApplication(ApplicationContext applicationContext, TranslationRunner translationRunner) {
+    public TranslatorApplication(ApplicationContext applicationContext, TranslationRunner translationRunner) {
         this.applicationContext = applicationContext;
         this.translationRunner = translationRunner;
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(CodesystemTranslatorApplication.class, args);
+        SpringApplication.run(TranslatorApplication.class, args);
     }
 
     @Override
